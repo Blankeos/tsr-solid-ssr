e6e9bf1eac4e791771567070d8feb1f9c4834375
Blankeos <carloantonioct@gmail.com>
2025-03-03 02:48:03 +0800

    feat: Got dynamic route loading to work.

--- src/routes/_layout/_dashboardLayout/dashboard/profiles/$id.tsx
+++ src/routes/_layout/_dashboardLayout/dashboard/profiles/$id.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute, useParams } from "@tanstack/solid-router";
+import { For } from "solid-js";
 
 export const Route = createFileRoute(
   "/_layout/_dashboardLayout/dashboard/profiles/$id",
 )({
   component: RouteComponent,
+  loader: async () => {
+    return [
+      {
+        id: "apple",
+        name: "Apple",
+      },
+      {
+        id: "google",
+        name: "Google",
+      },
+      {
+        id: "microsoft",
+        name: "Microsoft",
+      },
+    ];
+  },
 });
 
 function RouteComponent() {
@@ -11,10 +28,15 @@ function RouteComponent() {
     from: "/_layout/_dashboardLayout/dashboard/profiles/$id",
   });
 
+  const companies = Route.useLoaderData();
+
   return (
     <div>
       Hello "/_layout/_dashboardLayout/dashboard/profiles/$id"!
-      {JSON.stringify(params())}
+      <div>
+        Params: {JSON.stringify(params())}
+        <For each={companies()}>{(company) => <div>{company.name}</div>}</For>
+      </div>
     </div>
   );
 }
